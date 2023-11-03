1771bbe89bdba6a0b59129b881d09e956fba69ef
marimonon <mappi-@hotmail.co.jp>
2023-11-03 13:29:15 +0900

    画像の追加

--- docs/assets/js/common.js
+++ docs/assets/js/common.js
@@ -4,7 +4,6 @@
   var toggleDrawer = function toggleDrawer() {
     document.body.classList.toggle('is-drawer-active');
   };
-
   document.getElementById('drawer_btn').addEventListener('click', toggleDrawer);
   document.getElementById('drawer_shade').addEventListener('click', toggleDrawer);
 
